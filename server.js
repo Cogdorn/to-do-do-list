628e3383ef14baa95eb12c358a51b85e2e94ae19
Cogdorn <111153023+Cogdorn@users.noreply.github.com>
2023-05-18 12:03:11 -0700

    added cors

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
 require('dotenv').config(); 
+const cors = require('cors')
+
+
 
 
 
@@ -38,6 +41,7 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
   app.use(express.static('public'))
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
+  app.use(cors())
 
   // ========================
   // Routes
